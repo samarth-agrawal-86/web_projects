0a27cdace7012c16469622fb3079b09055daaac2
Samarth Agrawal <samarth.agrawal.86@gmail.com>
2022-05-14 13:17:42 +0530

    31. Rest api complete

--- wiki/app.js
+++ wiki/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require("lodash");
 
 const app = express();
 
@@ -24,7 +25,8 @@ app.get('/', function (req, res) {
     res.send('Hello');
 });
 
-app.get('/articles', function (req, res) {
+app.route('/articles')
+.get(function (req, res) {
     Article.find({},function (err, articles) {
         if (err) {
             console.log(err);
@@ -32,9 +34,8 @@ app.get('/articles', function (req, res) {
             res.send(articles);
         }
     });
-});
-
-app.post("/articles", function (req, res) {
+})
+.post(function (req, res) {
     let title = req.body.title;
     let content = req.body.content;
 
@@ -50,9 +51,8 @@ app.post("/articles", function (req, res) {
             res.send("Successfully added a new article");
         }
     });
-});
-
-app.delete('/articles', function (req, res) {
+})
+.delete(function (req, res) {
     Article.deleteMany({}, function (err) {
         if (err) {
             console.log(err);
@@ -61,7 +61,56 @@ app.delete('/articles', function (req, res) {
             res.send("Successfully Deleted all articles");
         }
     });
+});
+
+app.route("/articles/:articleName")
+.get(function (req, res) {
+    Article.findOne({title:req.params.articleName}, function (err, article) {
+        if (article) {
+            res.send(article);
+        } else {
+            res.send("No Article Found");
+        }
+    });
 })
+.put(function (req, res) {
+    Article.replaceOne(
+        {title:req.params.articleName},
+        {title: req.body.title, content:req.body.content},
+        function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send("PUT : record updated successfully");
+            }
+        }
+    )
+})
+.patch(function (req, res) {
+    Article.updateOne(
+        {title:req.params.articleName},
+        {title:req.body.title, content: req.body.content},
+        function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send("PATCH : record updated successfully");
+            }
+        }
+    );
+})
+.delete(function (req, res) {
+    Article.deleteOne(
+        {title: req.params.articleName},
+        function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send("DELETE : record deleted successfully");
+            }
+        }
+    );
+});
 
 let port = process.env.PORT;
 if(port==null || port==""){
