31cc89d089053dded3452967e83576ac627d4d4d
Samarth Agrawal <samarth.agrawal.86@gmail.com>
2022-05-19 14:14:11 +0530

    33. react components

--- keeper-app/src/App.jsx
+++ keeper-app/src/App.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Note from './Note';
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
 
 function App() {
-    return (
-        <div>
-        <Header />
-        <Note />
-        <Footer />
-        
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
