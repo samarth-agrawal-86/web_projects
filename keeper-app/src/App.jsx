055a118480bbea372890fcabd391e69cbc575164
Samarth Agrawal <samarth.agrawal.86@gmail.com>
2022-05-22 13:56:11 +0530

    33. keper app

--- keeper-app/src/App.jsx
+++ keeper-app/src/App.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
+import notes from "./Notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(function (noteItem) {
+        return (
+          <Note
+            key={noteItem.key}
+            title={noteItem.title}
+            content={noteItem.content}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
---
+++ keeper-app/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer(params) {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright {year}</p>
+    </footer>
+  );
+}
+
+export default Footer;
