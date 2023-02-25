724cbb44ddabfb9c8a17658a3ff36513ac1ae1af
Samarth Agrawal <samarth.agrawal.86@gmail.com>
2023-02-25 13:56:28 +0530

    minor change in lamadev

--- react-mui-lamadev/src/components/Navbar.jsx
+++ react-mui-lamadev/src/components/Navbar.jsx
@@ -48,7 +48,7 @@ function Navbar() {
               },
             }}
           >
-            LAMADEV
+            SOCIAL MEDIA
           </Typography>
           <PetsOutlined
             size="large"
